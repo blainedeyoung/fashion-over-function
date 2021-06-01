3133a4a08d65c9bd649317ea0283272c09b40ba9
blainedeyoung <45204685+blainedeyoung@users.noreply.github.com>
2021-06-01 06:18:24 -0400

    working on gallery images

--- app.js
+++ app.js
@@ -12,47 +12,9 @@
 
 //this script will initialize popovers
 //you can use basically this same script initialize tooltips
-//just by replacing 'popover' with 'tooltip' 
-const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover-hover"]'));
-const popoverList = popoverTriggerList.map((popoverTriggerEl) => {
-  return new bootstrap.Popover(popoverTriggerEl)
-});
+//just by replacing 'popover' with 'tooltip'
+// const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover-hover"]'));
+// const popoverList = popoverTriggerList.map((popoverTriggerEl) => {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// });
 
-//this is the span that will hold the power powerWords
-const powerWordSpan = document.getElementById('powerWords');
-const powerWords = ['Elegant', 'Professional', 'Sharp', 'Sensual', 'Fun', 'Classic'];
-
-//image box images grouped into an array
-const unsplash1 = document.getElementById('unsplash1');
-const unsplash2 = document.getElementById('unsplash2');
-const unsplash3 = document.getElementById('unsplash3');
-const unsplash4 = document.getElementById('unsplash4');
-const unsplash5 = document.getElementById('unsplash5');
-const unsplash6 = document.getElementById('unsplash6');
-const centerImages = [unsplash1, unsplash2, unsplash3, unsplash4, unsplash5, unsplash6];
-
-//this function will set the z-index of one of the centerImages above to 1
-//and start a timer that will call this again to set the next one in sequence
-function setImage(n) {
-  centerImages[n].style.zIndex = 1;
-  powerWordSpan.innerHTML = powerWords[n];
-  setTimeout( () => {
-    powerWordSpan.innerHTML = '';
-  }, 2000);
-  if (n === centerImages.length - 1) {
-    setTimeout ( () => {
-      setImage(0);
-      centerImages[n].style.zIndex = 0;
-    }, 10000);
-  } else {
-    setTimeout ( () => {
-      setImage(n+1);
-      centerImages[n].style.zIndex = 0;
-    }, 10000);
-  }
-};
-
-window.onload = setTimeout( () => {
-  setImage(0);
-}, 2000
-);
